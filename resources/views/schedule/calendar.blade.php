2f36b48ee7f99789d6d0d0202526e5abea93708a
anjemo2903 <molinajesus2003@gmail.com>
2024-12-01 18:44:22 -0600

    Calendar view

--- resources/views/schedule/calendar.blade.php
+++ resources/views/schedule/calendar.blade.php
@@ -1,4 +1,3 @@
-<!-- component -->
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -30,6 +29,28 @@
         </div> -->
 
         <div class="bg-white rounded-lg shadow overflow-hidden">
+        <div class="flex items-center justify-between py-2 px-6">
+        <div class="flex space-x-2">
+            <button 
+                type="button"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                @click="handleInstantAction">
+                Instantánea
+            </button>
+            <button 
+                type="button"
+                class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                @click="addToQueue">
+                Agregar a cola
+            </button>
+          <button 
+                type="button"
+                class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
+                @click="addToQueue">
+                Configurar Horarios
+            </button>
+        </div>
+    </div>
 
             <div class="flex items-center justify-between py-2 px-6">
                 <div>
@@ -126,43 +147,63 @@
                 </svg>
             </div>
 
-            <div class="shadow w-full rounded-lg bg-white overflow-hidden w-full block p-8">
-                
-                <h2 class="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">Add Event Details</h2>
-             
-                <div class="mb-4">
-                    <label class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Event title</label>
-                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" x-model="event_title">
-                </div>
+            <div class="shadow w-full rounded-lg bg-white overflow-hidden w-full block p-8 max-h-[80vh] overflow-y-auto">
+            <h2 class="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">Program a post</h2>
+            <div class="text-gray-700 text-lg font-semibold mb-4">
+                Selected Date: <span class="text-blue-500" x-text="event_date"></span>
+            </div>
+            
+            <div class="mb-4">
+                <label class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Post description</label>
+                <textarea 
+                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full h-32 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" 
+                    x-model="event_title">
+                </textarea>
+            </div>
 
+            <div class="mb-4">
                 <div class="mb-4">
-                    <label class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Event date</label>
-                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" x-model="event_date" readonly>
+                    <label class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Event time</label>
+                    <input 
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" 
+                        type="time" 
+                        x-model="event_time"
+                    >
                 </div>
+            </div>
 
-                <div class="inline-block w-64 mb-4">
-                    <label class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Select a theme</label>
-                    <div class="relative">
-                        <select @change="event_theme = $event.target.value;" x-model="event_theme" class="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700">
-                                <template x-for="(theme, index) in themes">
-                                    <option :value="theme.value" x-text="theme.label"></option>
-                                </template>
-                            
-                        </select>
-                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                        </div>
+            <div class="inline-block w-64 mb-4">
+                <label class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Select a social media</label>
+                <div class="relative">
+                    <select 
+                        @change="event_theme = $event.target.value;" 
+                        x-model="event_theme" 
+                        class="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700">
+                        <option value="twitter" x-text="'Twitter'">Twitter</option>
+                        <option value="mastodon" x-text="'Mastodon'">Mastodon</option>
+                        <option value="ambas" x-text="'Ambas'">Ambas</option>
+                    </select>
+                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                        </svg>
                     </div>
                 </div>
+            </div>
 
-                <div class="mt-8 text-right">
-                    <button type="button" class="bg-white hover:bg-gray-100 text-gray-700 font-semibold py-2 px-4 border border-gray-300 rounded-lg shadow-sm mr-2" @click="openEventModal = !openEventModal">
-                        Cancel
-                    </button>	
-                    <button type="button" class="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-2 px-4 border border-gray-700 rounded-lg shadow-sm" @click="addEvent()">
-                        Save Event
-                    </button>	
-                </div>
+            <!-- Botones al final -->
+            <div class="mt-8 text-right">
+                <button type="button" 
+                        class="bg-white hover:bg-gray-100 text-gray-700 font-semibold py-2 px-4 border border-gray-300 rounded-lg shadow-sm mr-2" 
+                        @click="openEventModal = !openEventModal">
+                    Cancel
+                </button>	
+                <button type="button" 
+                        class="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-2 px-4 border border-gray-700 rounded-lg shadow-sm" 
+                        @click="addEvent()">
+                    Save Post
+                </button>	
+    </div>
             </div>
         </div>
     </div>
